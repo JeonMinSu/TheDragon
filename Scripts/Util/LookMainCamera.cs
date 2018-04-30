f421ca3e16270daaf556040f6bd40526bf0c1d7d
SamMul <815youngmin@naver.com>
2018-04-30 18:42:40 +0900

    씬 합친거

---
+++ Scripts/Util/LookMainCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookMainCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dir = Camera.main.transform.position - this.transform.position;
+        transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
+	}
+}
